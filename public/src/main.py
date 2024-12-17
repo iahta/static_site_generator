895e3bda403d4045f3b578ceedb60c64646f9166
iahta <153889016+iahta@users.noreply.github.com>
2024-12-16 22:53:13 -0600

    added textnode

---
+++ public/src/main.py
@@ -0,0 +1,16 @@
+from textnode import TextNode
+
+def main():
+    new_node = TextNode("This is a text node", "bold", "https://www.boots.dev")
+    print(new_node)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
