f0a90416b48002715023b1d3b9820f33a1d21503
iahta <153889016+iahta@users.noreply.github.com>
2024-12-25 22:17:57 -0600

    "main now copys static"

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from textnode import TextNode
+import os
+import shutil
 
 def main():
-    new_node = TextNode("This is a text node", "bold", "https://www.boots.dev")
-    print(new_node)
-
-
-
-
-
+    copy_static("static", "public")
+ 
+def copy_static(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+ 
+    os.mkdir(dest_dir)
+    
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+        
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+        else:
+            copy_static(source_path, dest_path)
 
 
 
